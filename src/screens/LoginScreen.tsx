0bc3dd14d8d14c01316e3de901cdc0811491645e
Natnael Biruk <natnael.biruk@lersha.com>
2023-05-22 20:09:18 +0300

    more ui improvements on the login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -6,19 +6,17 @@ const LoginScreen = () => {
   const navigation: any = useNavigation();
 
   const [text, setText] = useState("");
+  const [placeholder, setPlaceholder] = useState("Enter your pin");
 
-  const clearText = () => {
-    setText("");
-  };
   return (
     <View className="flex-1 items-center justify-around bg-slate-900 p-8 pt-12 gap-2">
       {/* Section 1 */}
-      <View className="flex items-center gap-4">
+      <View className="flex space-y-12 items-center">
         <Image
           source={require("../../assets/icon.png")}
           className="h-20 w-20"
         />
-        <View>
+        <View className="flex items-center">
           <Text className="text-white text-2xl font-bold">Welcome!</Text>
           <Text className="text-white text-lg">Lucy Finance</Text>
         </View>
@@ -27,20 +25,21 @@ const LoginScreen = () => {
       {/* Section 2 */}
       <View className="flex items-center justify-center gap-6 p-5">
         <TextInput
-          className="bg-slate-700 w-1/2 text-white text-center text-xl px-12 py-2 rounded-md"
+          className="bg-slate-700 w-1/2 text-white text-center text-xl px-14 py-2 rounded-md"
           maxLength={6}
-          placeholder="Enter your pin"
+          placeholder={placeholder}
+          onFocus={() => setPlaceholder("")}
           placeholderTextColor={"white"}
           keyboardType="number-pad"
           clearTextOnFocus={true}
           secureTextEntry
           value={text}
-          onChangeText={(e) => setText(e)}
+          onChangeText={(text) => setText(text)}
         />
         <TouchableOpacity
           className="bg-slate-700 py-4 px-8 rounded-lg"
           onPress={() => {
-            navigation.navigate("Home"), setText("");
+            setText(""), navigation.navigate("Home");
           }}
         >
           <Text className="text-white">Login</Text>
